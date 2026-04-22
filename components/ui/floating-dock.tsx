a74f3fbaaf21cbb207b7f735459caca30680b068
ashishp05 <hariparaashish6686@gmail.com>
2026-04-22 23:08:56 +0530

    changes

--- components/ui/floating-dock.tsx
+++ components/ui/floating-dock.tsx
@@ -71,7 +71,7 @@ const FloatingDockMobile = ({
                 <a
                   href={item.href}
                   key={item.title}
-                   target="_blank"
+                  target="_blank"
                   className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-50 dark:bg-neutral-900"
                 >
                   <div className="h-4 w-4">{item.icon}</div>
